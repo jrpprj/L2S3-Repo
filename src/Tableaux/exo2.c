05c653aa50d9d1111c8e6d7dc588a6c8c7ba1f04
Bibi <patrick.riviere--jombart@univ-tlse3.fr>
2019-10-03 16:47:26 +0000

    Modification 2.0

---
+++ src/Tableaux/exo2.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#define TAILLE 100000
+
+int crible(int * f)
+{
+    f[0] = 0;
+    f[1] = 1;
+    for (int i = 2; i < TAILLE; i++)
+    {
+        f[i] = i;
+    }
+    return 0;
+}
+
+int cribleEr(int * f)
+{
+    int j;
+    for (int i = 0; (i*i) < TAILLE; i++)
+    {
+        if (f[i] == i)
+        {
+            for (j = 2; i*j < TAILLE; j++)
+            {
+                f[i*j] = j;   
+            }
+        }   
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    int t[TAILLE];
+    crible(t);
+    cribleEr(t);
+    int i = 0;
+    int n;
+    while (i == 0)
+    {
+        scanf("%d", &n);
+        if (n < 0)
+        {
+            break;
+        }
+        if (n >= TAILLE)
+        {
+            continue;
+        }
+        else
+        {
+            if (t[n] == n && n != 1)
+            {
+                printf("%d est un nombre premier", n);
+            }
+            else
+            {
+                printf("%d n'est pas un nombre premier", n);
+            }
+        }
+    }
+}
